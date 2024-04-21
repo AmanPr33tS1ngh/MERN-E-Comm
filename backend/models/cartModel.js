5027d570433c70598fd6ceb9fff70b36df3b4c21
AmanPr33tS1ngh <amanpr333tsingh01@gmail.com>
2024-04-21 21:50:55 +0530

    api changes

--- backend/models/cartModel.js
+++ backend/models/cartModel.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
-const cartSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+const cartItemSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -53,5 +52,10 @@ const cartSchema = new mongoose.Schema({
   },
 });
 
-const Cart = mongoose.model("Cart", cartSchema);
-export default Cart;
+const cartSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  cartItems: [cartItemSchema],
+});
+
+module.exports = mongoose.model("Cart", cartSchema);
+module.exports = mongoose.model("CartItem", cartItemSchema);
--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,18 +1,21 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
-import Cart from "../models/cartModel.js";
+import { Cart, CartItems } from "../models/cartModel.js";
 import User from "../models/userModel.js";
 import { isAuth } from "../utils.js";
 
 const cartRouter = express.Router();
 
-cartRouter.get("/", async (req, res) => {
-  const products = await Cart.find();
-  res.send(products);
+cartRouter.get("/", isAuth, async (req, res) => {
+  const cart = await Cart.findOne({ user: req.user._id });
+  if (!cart) {
+    return res.send({ success: false, msg: "Cart not found", cart: [] });
+  }
+  res.send({ success: true, msg: "Cart found", cart: cart });
 });
 
 cartRouter.get("/:id", async (req, res) => {
-  const product = await Cart.findById(req.params.id);
+  const product = await Cart.findOne(req.params.id);
 
   if (product) {
     res.send(product);
@@ -25,60 +28,69 @@ cartRouter.post(
   "/",
   isAuth,
   expressAsyncHandler(async (req, res) => {
-    const newCart = new Cart({
-      user: req.user._id,
-      name: req.body.name,
-      slug: req.body.slug,
-      image: req.body.image,
-      brand: req.body.brand,
-      category: req.body.category,
-      description: req.body.description,
-      price: req.body.price,
-      rating: req.body.rating,
-      quantity: req.body.quantity,
-      numReviews: req.body.numReviews,
-      countInStock: req.body.countInStock,
-      totalPrice: req.body.totalPrice,
-    });
-
-    const cart = await newCart.save();
-    res.status(201).send({ message: "New ORder created: ", cart });
+    try {
+      const cartItem = new CartItems({
+        name: req.body.name,
+        slug: req.body.slug,
+        image: req.body.image,
+        brand: req.body.brand,
+        category: req.body.category,
+        description: req.body.description,
+        price: req.body.price,
+        rating: req.body.rating,
+        quantity: req.body.quantity,
+        numReviews: req.body.numReviews,
+        countInStock: req.body.countInStock,
+        totalPrice: req.body.totalPrice,
+      });
+      let userCart = await findOne({
+        user: req.user._id,
+      });
+      if (!userCart) {
+        userCart = new Cart({
+          user: req.user._id,
+        });
+      }
+      userCart.cartItems.unshift(cartItem);
+      const cart = await userCart.save();
+      res.send({ success: true, message: "New cartItem added: ", cart: cart });
+    } catch (e) {
+      res.send({ success: false, message: `Err while adding cartItem ${e}` });
+    }
   })
 );
 
 cartRouter.put(
   "/:id/:quantity",
   expressAsyncHandler(async (req, res) => {
-    const id = req.params.id;
+    const slug = req.params.slug;
     const quantity = req.params.quantity;
 
-    const cart = await Cart.findById(id);
-
-    cart.quantity = quantity;
-    cart.totalPrice = req.body.totalPrice;
-
-    const updated = await cart.save();
-
-    res.status(201).send({ message: " ORder updated: ", updated });
+    let cart = await Cart.findOne({
+      user: req.user._id,
+    });
+    if (!cart) {
+      res.send({ success: false, message: "Order not updated" });
+    }
+    const cartItemToBeUpdated = cart.cartItems.filter(
+      (cartItem) => cartItem.slug === slug
+    );
+    cartItemToBeUpdated.quantity = quantity;
+    cartItemToBeUpdated.totalPrice = cartItemToBeUpdated.price * quantity;
+    const updatedCart = await cart.save();
+    res.send({ success: true, message: " Order updated: ", updatedCart });
   })
 );
 
 cartRouter.delete(
   "/",
+  isAuth,
   expressAsyncHandler(async (req, res) => {
-    const products = await Cart.deleteMany();
-    console.log(products);
-    res.status(201).send({ message: " ORders delted: ", products });
+    const cart = await Cart.findOne({ user: req.user._id });
+    cart.cartItems = [];
+    const updatedCart = await cart.save();
+    res.send({ message: " Orders delted: ", updatedCart: updatedCart });
   })
 );
-cartRouter.delete(
-  "/:id",
-  expressAsyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const cart = await Cart.findById(id);
-    const products = await Cart.deleteOne(cart);
-    console.log(cart);
-    res.status(201).send({ message: "ORder deleted: ", cart });
-  })
-);
+
 export default cartRouter;
